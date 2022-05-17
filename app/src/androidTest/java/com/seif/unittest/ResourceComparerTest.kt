e3dfd5f4841edd5e35cfbcbad9d9c76a589e52db
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-05-17 16:53:07 +0200

    Task 2 ( @After adn @Before )

--- app/src/androidTest/java/com/seif/unittest/ResourceComparerTest.kt
+++ app/src/androidTest/java/com/seif/unittest/ResourceComparerTest.kt
@@ -1,5 +1,37 @@
 package com.seif.unittest
 
-import org.junit.Assert.*
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
 
-class ResourceComparerTest
+class ResourceComparerTest {
+    //  val resourceComparer =  ResourceComparer()   bad practice to init this object global bec as we know each test case must be independent from each other
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setUp() { // will run before each test case
+        resourceComparer = ResourceComparer()
+    }
+
+    @After
+    fun tearUp() { // will run after each test case and used for end or reset objects
+
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val actual = resourceComparer.isEqual(context, R.string.app_name, "Unit Test")
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentFromGivenString_returnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val actual = resourceComparer.isEqual(context, R.string.app_name, "seif")
+        assertThat(actual).isFalse()
+    }
+}
